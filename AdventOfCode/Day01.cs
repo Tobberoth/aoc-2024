388dfd7e9d2e6f0c4d089afe3776bbc409a961e2
Tobias Dersén <tobberoth@gmail.com>
2024-12-02 12:00:03 +0900

    Day1 correct answers

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -4,12 +4,34 @@ public class Day01 : BaseDay
 {
     private readonly string _input;
 
+    private readonly List<int> A = [];
+    private readonly List<int> B = [];
+
     public Day01()
     {
         _input = File.ReadAllText(InputFilePath);
+        foreach (var part in _input.Split('\n')) {
+            var parts = part.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            A.Add(int.Parse(parts[0]));
+            B.Add(int.Parse(parts[1]));
+        }
+        A.Sort();
+        B.Sort();
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1() {
+        var diffSum = 0;
+        for (var i = 0; i < A.Count; i++) {
+            diffSum += Math.Abs(A[i] - B[i]);
+        }
+        return new ValueTask<string>($"{diffSum}");
+    }
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    public override ValueTask<string> Solve_2() {
+        long bigInt = 0;
+        foreach (var num in A) {
+            bigInt += num * B.Where(b => b == num).Count();
+        }
+        return new($"{bigInt}");
+    }
 }
